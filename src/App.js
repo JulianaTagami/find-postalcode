4d74f035a4fb37b094a9d90d7a209b318008c430
JulianaTagami <jutagamiss@gmail.com>
2022-06-29 16:31:18 +0100

    add address line2

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ function App() {
           <h2>CEP: {cep.cep}</h2>
 
           <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento} </span>
           <span>{cep.bairro}</span>
           <span>{cep.localidade} - {cep.uf}</span>
         </main>
